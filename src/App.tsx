d2201a3aa58aa8ad0fc8cb452db10ef77a0f40da
HerbertHe <Herbert.He0229@gmail.com>
2023-11-21 12:52:15 +0800

    feat :sparkles: : support discord

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,47 @@
-import Giscus from "@giscus/react";
-import "./App.less";
-import avatar from "./assets/avatar.jpeg";
+import Giscus from "@giscus/react"
+import "./App.less"
+import avatar from "./assets/avatar.jpeg"
 
 const App = () => {
-  return (
-    <div className="App">
-      <div className="idea-text">
-        <div className="idea-avatar">
-          <img src={avatar} alt="avatar" />
+    return (
+        <div className="App">
+            <div className="idea-text">
+                <div className="idea-avatar">
+                    <img src={avatar} alt="avatar" />
+                </div>
+                <div className="idea-desc">
+                    <header className="text-3xl font-bold">
+                        Share idea with me!
+                    </header>
+                    <p>Amazing project comes from amazing idea! 💡</p>
+                    <p>plz leave your idea here! 😯</p>
+                    <p>
+                        Or, share with{" "}
+                        <a href="https://discord.gg/CVnjZXNM">Discord</a> ✌️
+                    </p>
+                    <p>Or, email to me</p>
+                    <a href="mailto:hi@ibert.me">hi@ibert.me</a>
+                </div>
+            </div>
+            <div className="idea-comments">
+                <Giscus
+                    id="comments"
+                    repo="HerbertHe/idea"
+                    repoId="R_kgDOIojDbQ"
+                    category="Ideas"
+                    categoryId="DIC_kwDOIojDbc4CTH7M"
+                    mapping="pathname"
+                    strict="0"
+                    reactionsEnabled="1"
+                    emit-metadata="0"
+                    input-position="top"
+                    term="Welcome to share idea with me!"
+                    theme="preferred_color_scheme"
+                    lang="en"
+                />
+            </div>
         </div>
-        <div className="idea-desc">
-          <header className="text-3xl font-bold">Share idea with me!</header>
-          <p>Amazing project comes from amazing idea! 💡</p>
-          <p>plz leave your idea here! 😯</p>
-          <p>Or, you can email to me</p>
-          <a href="mailto:hi@ibert.me">hi@ibert.me</a>
-        </div>
-      </div>
-      <div className="idea-comments">
-        <Giscus
-          id="comments"
-          repo="HerbertHe/idea"
-          repoId="R_kgDOIojDbQ"
-          category="Ideas"
-          categoryId="DIC_kwDOIojDbc4CTH7M"
-          mapping="pathname"
-          strict="0"
-          reactionsEnabled="1"
-          emit-metadata="0"
-          input-position="top"
-          term="Welcome to share idea with me!"
-          theme="preferred_color_scheme"
-          lang="en"
-        />
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default App;
+export default App
